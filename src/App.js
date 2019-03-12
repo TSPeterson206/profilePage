4ef0804ced51ceb764f9fa6a1fed3b2c45b63e57
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2019-03-11 22:31:52 -0700

    made columns, added transitions, added icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
+
+
+connectGit(){
+
+}
+
   render() {
     return (
+      <div className="row mainColumns">
+      <div className="col-2">
+      <header className="columnHeader">About</header>
+      </div>
+      <div className="col-2">
+      <header className="columnHeader">Resume</header>
+      </div>
+
+      <div className="col-4 titleColumn">
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p className="titleName titles">Toby Peterson</p>
+         <p className="titleTitle titles">Software Engineer</p>
+      <img className="headshot" src="https://tobypeterson.surge.sh/Toby_Peterson.png" alt="headshot"></img>
+      </div>
+      </div>
+      <div className="col-2">
+      <header className="columnHeader">Projects</header>
+      </div>
+      <div className="col-2">
+      <header className="columnHeader">Contact</header>
+      <div>
+      
+        <a href="https://github.com/TSPeterson206"><img class="profileContactLogo" src="./images/icons8-github-90.png"/></a>
+
+        <a href="https://www.linkedin.com/in/toby-peterson"><img class="profileContactLogo" src="icons8-linkedin-90.png"/></a>
+        <a href="mailto:tspeterson206@gmail.com"><img class="profileContactLogo" src="icons8-gmail-90.png"/></a>
+        
+        {/* <a href="https://www.linkedin.com/in/toby-peterson"><img className="profileContactLogo" src="icons8-linkedin-90.png"></a> */}
+        {/* <a href="mailto:tspeterson206@gmail.com"><img classname="profileContactLogo" src="icons8-gmail-90.png"></a> */}
+      </div>
+      </div>
       </div>
     );
   }
